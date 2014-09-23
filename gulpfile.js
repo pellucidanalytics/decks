1645167a75cc2fefc8172e4c94d648097fed4bb8
Andy White <andy.white@pellucid.com>
2014-09-23 11:31:47 -0600

    Add watch tasks for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,46 @@
 var browserify = require("browserify");
 var gulp = require("gulp");
+var gutil = require("gutil");
 var mochaPhantomJS = require("gulp-mocha-phantomjs");
 var nib = require("nib");
+var path = require("path");
 var source = require("vinyl-source-stream");
 var stylus = require("gulp-stylus");
 var watchify = require("watchify");
 
 var paths = {
   src: {
-    jsMain: "lib/index.js",
-    jsAll: "lib/**/*.js",
+    jsMain: "./lib/index.js",
+    jsAll: "./lib/**/*.js",
 
-    stylMain: "lib/index.styl",
-    stylAll: "lib/**/*.styl",
+    stylMain: "./lib/index.styl",
+    stylAll: "./lib/**/*.styl",
 
-    out: "dist",
+    out: "./dist",
   },
   test: {
-    htmlMain: "test/index.html",
-    htmlAll: "test/**/*.html",
+    htmlMain: "./test/index.html",
+    htmlAll: "./test/**/*.html",
 
-    jsMain: "test/index.js",
-    jsAll: "test/**/*.js",
+    jsMain: "./test/index.js",
+    jsAll: "./test/**/*.js",
 
-    stylMain: "test/index.styl",
-    stylAll: "test/**/*.styl",
+    stylMain: "./test/index.styl",
+    stylAll: "./test/**/*.styl",
 
-    out: "dist/test"
+    out: "./dist/test"
   },
   example: {
-    htmlMain: "example/index.html",
-    htmlAll: "example/**/*.html",
+    htmlMain: "./example/index.html",
+    htmlAll: "./example/**/*.html",
 
-    jsMain: "example/index.js",
-    jsAll: "example/**/*.js",
+    jsMain: "./example/index.js",
+    jsAll: "./example/**/*.js",
 
-    stylMain: "example/index.styl",
-    stylAll: "example/**/*.styl",
+    stylMain: "./example/index.styl",
+    stylAll: "./example/**/*.styl",
 
-    out: "dist/example"
+    out: "./dist/example"
   }
 };
 
@@ -79,41 +81,66 @@ gulp.task("example", ["html-example", "styl-example", "js-example"]);
 // test tasks
 ////////////////////////////////////////////////////////////////////////////////
 
+// Copy .html files to test output folder
 gulp.task("html-test", function() {
   gulp.src(paths.test.htmlMain)
     .pipe(gulp.dest(paths.test.out));
 });
 
+// Compile .styl files to .css files in test output folder
 gulp.task("styl-test", function() {
   gulp.src(paths.test.stylMain)
     .pipe(stylus({ use: nib() }))
     .pipe(gulp.dest(paths.test.out));
 });
 
+// Bundle .js files into test output folder
 gulp.task("js-test", function() {
-  return browserify("./test/index.js")
+  return browserify(paths.test.jsMain)
     .bundle()
     .pipe(source("bundle.js"))
     .pipe(gulp.dest(paths.test.out));
 });
 
+// Run mocha PhantomJS tests in test output folder
 gulp.task("mocha-test", function() {
-  return gulp.src("dist/test/index.html")
+  return gulp.src(path.join(paths.test.out, "index.html"))
     .pipe(mochaPhantomJS());
 });
 
+// Watch test .html files
 gulp.task("watch-html-test", function() {
   gulp.watch(paths.test.htmlAll, ["html-test"]);
 });
 
+// Watch test .styl files
 gulp.task("watch-styl-test", function() {
   gulp.watch(paths.test.stylAll, ["styl-test"]);
 });
 
+// Watch test .js files
 gulp.task("watch-js-test", function() {
+  var bundler = watchify(browserify(paths.test.jsMain, watchify.args));
+  bundler.on("update", rebundle);
+  function rebundle() {
+    return bundler.bundle()
+      .on("error", gutil.log.bind(gutil, "browserify error"))
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest(paths.test.out));
+  }
+  return rebundle();
 });
 
+// Watch built test .js file
+gulp.task("watch-mocha-test", function() {
+  gulp.watch(path.join(paths.test.out, "index.js"), ["mocha-test"]);
+});
+
+// Run the test build
 gulp.task("test", ["html-test", "styl-test", "js-test", "mocha-test"]);
+
+// Start the watchers for test files
+gulp.task("watch-test", ["watch-html-test", "watch-styl-test", "watch-js-test", "watch-mocha-test"]);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Default task
