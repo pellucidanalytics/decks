9c08a90b8867bfab701eea824f750db0b5d6dc29
Andy White <andy.white@pellucid.com>
2014-09-25 10:40:14 -0600

    Horizontal scrolling demo

--- sandbox/scrolling/index.js
+++ sandbox/scrolling/index.js
@@ -7,8 +7,8 @@
     this.items = options.items; // array of decks "Item" objects - so Scroller can control load/unload
     this.itemWidth = options.itemWidth;
     this.itemHeight = options.itemHeight;
-    this.itemGutterSize = options.itemGutterSize; // (internal padding between items)
-    this.itemPaddingSize = options.itemPaddingSize;
+    this.itemGutterSize = options.itemGutterSize || 10; // (internal padding between items)
+    this.itemPaddingSize = options.itemPaddingSize || 10; // (padding around items)
 
     // Scroller parameters
     this.scrollDirection = options.scrollDirection || "h"; // h or v
@@ -25,43 +25,64 @@
     init: function() {
       var self = this;
 
-      self.$container.empty();
-
       // Scroller container with overflow hidden and width of container.
       // The items scroll within this container, and touch gestures are bound to this container
       self.$scrollerLevel1 = $("<div>")
         .addClass("scroller-level1")
         .css({
           width: self.$container.width(),
-          height: self.$container.height()
+          height: self.getTotalItemHeight(),
+          overflow: "hidden"
         })
         .appendTo(self.$container);
 
       // Scroller inner container with full width of items.
       // This container scrolls within the level 1 container.
+      // This container has position relative, so the items can be absolutely positioned within it.
       self.$scrollerLevel2 = $("<div>")
         .addClass("scroller-level2")
         .css({
-          width: self.itemWidth * items.length + self.itemGutterSize * (items.length + 1),
-          height: self.itemHeight + self.itemPaddingSize * 2
+          width: self.getTotalItemWidth(),
+          height: self.getTotalItemHeight(),
+          position: "relative"
         })
         .appendTo(self.$scrollerLevel1);
 
+      // Load the items, and insert them into the level 2 container with absolute positioning
+      _.each(self.items, function(item, i, items) {
+        var $item = $(item.load());
+
+        $item.css({
+          position: "absolute",
+          "pointer-events": "none",
+          top: self.itemPaddingSize + "px",
+          left: ((self.itemWidth * i) + (self.itemGutterSize * (i + 1))) + "px"
+        });
+
+        $item.appendTo(self.$scrollerLevel2);
+      });
+
+      // State variables
       self.isSwiping = false;
       self.startLeft = 0;
 
-      _.each(self.items, function(item) {
-        self.$scrollerLevel2.append(item.load());
-      });
-
+      // Bind UI events
       self.bind();
+    },
+
+    getTotalItemHeight: function() {
+      return this.itemHeight + this.itemPaddingSize * 2;
+    },
+
+    getTotalItemWidth: function() {
+      return (this.itemWidth * this.items.length) + (this.itemGutterSize * (this.items.length + 1));
     },
 
     /**
      * Binds events
      */
     bind: function() {
-      self.$scrollerLevel1.hammer();
+      this.$scrollerLevel1.hammer();
       this.$scrollerLevel1.on("pan", _.bind(this.onPan, this));
       this.$scrollerLevel1.on("tap press touchstart mousedown", _.bind(this.onTap, this));
       this.$scrollerLevel1.on("swipe", _.bind(this.onSwipe, this));
@@ -75,6 +96,7 @@
     },
 
     onPan: function(e) {
+      var self = this;
       console.log(e.type, e.gesture.deltaX, e.gesture.velocityX, e.gesture);
       if (self.isSwiping) {
         return;
@@ -87,11 +109,13 @@
     },
 
     onTap: function(e) {
+      var self = this;
       console.log(e.type);
       self.$scrollerLevel2.velocity("stop");
     },
 
     onSwipe: function(e) {
+      var self = this;
       console.log(e.type, e.gesture.deltaX, e.gesture.velocityX, e.gesture);
 
       // velocityX is positive for moving left, negative for moving right
@@ -101,13 +125,13 @@
       var easing = self.swipeEasing;
 
       self.isSwiping = true;
-      $level2.velocity({
+      self.$scrollerLevel2.velocity({
           "left": deltaLeft
         }, {
           duration: duration,
           easing: easing,
           complete: function() {
-            self.startLeft = parseInt($level2.css("left"));
+            self.startLeft = parseInt(self.$scrollerLevel2.css("left"));
             self.isSwiping = false;
           }
         });
@@ -115,83 +139,65 @@
   });
 
   $(function() {
+    var $window = $(window);
     var $body = $("body");
-    var $level1 = $("#level1");
-    var $level2 = $("#level2");
+    var $root = $("#root");
 
     var itemCount = 10;
-    var width = 300;
-    var height = 200;
-    var margin = 10;
+    var itemWidth = 300;
+    var itemHeight = 200;
+    var itemGutterSize = 10;
+    var itemPaddingSize = 10;
 
-    var startLeft = 10;
-    var startTop = 10;
+    var rowCount = 20;
 
-    var isSwiping = false;
+    $root.width($window.width());
+    $root.height(rowCount * (itemHeight + 2 * itemPaddingSize));
 
-    $level1.width($(window).width() - margin * 2);
-    $level1.height(height + margin * 2);
 
-    $level2.height(height + margin * 2);
-    $level2.width(width * itemCount + margin * (itemCount + 1));
+    var horizontalScrollers = _.map(_.range(rowCount), function(rowIndex) {
+      // Add the images to #root
+      var items = _.map(_.range(itemCount), function(i) {
+        var image = new Image(itemWidth, itemHeight);
+        image.src = "http://lorempixel.com/" + itemWidth + "/" + itemHeight + "/";
 
-    // Add the images to #root
-    _.each(_.range(itemCount), function(i) {
-      var image = new Image(width, height);
-      image.src = "http://lorempixel.com/" + width + "/" + height + "/";
-
-      image.style.top = "10px";
-      image.style.left = ((width * i) + (margin * (i + 1))) + "px";
-
-      $level2.append(image);
-    });
-
-    // Setup touch events on the level1 container
-    $level1.hammer();
-
-    $level1.on("pan", function(e) {
-      console.log(e.type, e.gesture.deltaX, e.gesture.velocityX, e.gesture);
-
-      if (isSwiping) {
-        return;
-      }
-
-      var newLeft = startLeft + e.gesture.deltaX;
-      $level2.css("left", newLeft);
-      if (e.gesture.isFinal) {
-        startLeft = newLeft;
-      }
-    });
-
-    $level1.on("tap press touchstart mousedown", function(e) {
-      console.log(e.type);
-      $level2.velocity("stop");
-    });
-
-    $level1.on("swipe", function(e) {
-      console.log(e.type, e.gesture.deltaX, e.gesture.velocityX, e.gesture);
-
-      // velocityX is positive for moving left, negative for moving right
-      var distanceMultiplier = 200;
-      var durationMultiplier = 100;
-      var distanceX = e.gesture.velocityX * distanceMultiplier;
-      var deltaLeft = "-=" + (distanceX) + "px";
-      var duration = Math.abs(e.gesture.velocityX * durationMultiplier);
-      //var easing = "easeOutCubic";
-      //var easing = "easeOutElastic";
-      var easing = [200, 20];
-
-      isSwiping = true;
-      $level2.velocity({
-          "left": deltaLeft
-        }, {
-          duration: duration,
-          easing: easing,
-          complete: function() {
-            startLeft = parseInt($level2.css("left"));
-            isSwiping = false;
+        // simulate the "Item" class
+        return {
+          load: function() {
+            return image;
           }
-        });
+        };
+      });
+
+      return new Scroller({
+        container: $root,
+        items: items,
+        itemWidth: itemWidth,
+        itemHeight: itemHeight,
+        itemGutterSize: itemGutterSize,
+        itemPaddingSize: itemPaddingSize,
+        scrollDirection: "h",
+        swipeEasing: "easeOutExpo"
+        //swipeEasing: [500, 10]
+      });
+    });
+
+    // TODO: need to implement vertical scrolling with touch gestures
+    var verticalScroller = new Scroller({
+      container: $root,
+      items: _.map(horizontalScrollers, function(horizontalScroller) {
+        return {
+          load: function() {
+            return horizontalScroller.container;
+          }
+        };
+      }),
+      itemWidth: horizontalScroller.getTotalItemWidth(),
+      itemHeight: itemHeight,
+      itemGutterSize: itemGutterSize,
+      itemPaddingSize: itemPaddingSize,
+      scrollDirection: "v",
+      swipeEasing: "easeOutExpo"
     });
 
   });
