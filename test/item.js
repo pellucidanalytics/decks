875107b6591b8fa1e9ec7b67cf3a4198828305ff
Andy White <andy.white@pellucid.com>
2014-09-30 13:26:00 -0600

    item tests

--- test/item.js
+++ test/item.js
@@ -50,7 +50,86 @@ describe("item", function() {
 
       item.on("changed", spy);
 
-      //item.set("key1", "val
+      item.set("key1", "val2");
+      spy.should.have.been.calledWith({ key: "key1", oldValue: "val1", newValue: "val2" });
+      spy.reset();
+
+      item.set("key1", null);
+      spy.should.have.been.calledWith({ key: "key1", oldValue: "val2", newValue: null });
+      spy.reset();
+
+      item.set("key2", "newVal");
+      spy.should.have.been.calledWith({ key: "key2", oldValue: undefined, newValue: "newVal" });
+      spy.reset();
+    });
+
+    it("should do nothing if the value is not changing, and not using options.force = true", function() {
+      var item = new Item({ key1: "val1" });
+      var spy = sinon.spy();
+
+      item.on("changed", spy);
+
+      item.set("key1", "val1");
+      spy.should.have.callCount(0);
+      item.get("key1").should.eql("val1");
+      spy.reset();
+
+      item.set("key1", "val1", { silent: true });
+      spy.should.have.callCount(0);
+      item.get("key1").should.eql("val1");
+      spy.reset();
+    });
+
+    it("should force the set if using options.force = true", function() {
+      var item = new Item({ key1: "val1" });
+      var spy = sinon.spy();
+      item.on("changed", spy);
+
+      item.set("key1", "val1", { force: true, silent: false });
+      spy.should.have.been.called;
+      item.get("key1").should.eql("val1");
+      spy.reset();
+
+      item.set("key1", "val1", { force: true, silent: true }); // Not really a useful way to call .set
+      spy.should.have.callCount(0);
+      item.get("key1").should.eql("val1");
+      spy.reset();
+    });
+
+    it("should not emit events for options.silent = true", function() {
+      var item = new Item({ key1: "val1" });
+      var spy = sinon.spy();
+
+      item.on("changed", spy);
+
+      item.set("key1", "val2", { silent: true });
+      spy.should.have.callCount(0);
+      item.get("key1").should.eql("val2");
+      spy.reset();
+    });
+  });
+
+  describe("hasRender", function() {
+    it("should return false for non-existent renders", function() {
+      var item = new Item();
+      item.hasRender().should.be.false;
+      item.hasRender(null).should.be.false;
+      item.hasRender(undefined).should.be.false;
+      item.hasRender("key1").should.be.false;
+    });
+
+    it("should return true for existing renders", function() {
+      var item = new Item();
+      item.setRender("key1", { element: "something", transform: "something" });
+      item.hasRender("key1").should.be.true;
+    });
+  });
+
+  describe("getRender", function() {
+    it("should return undefined for non-existent renders", function() {
+      var item = new Item();
+
+      expect(item.getRender("key1")).to.be.undefined;
     });
   });
 
