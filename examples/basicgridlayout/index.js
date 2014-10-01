3b5648295a1e29990117befe1ecb33f1a3cf30d2
Andy White <andy.white@pellucid.com>
2014-10-01 16:29:18 -0600

    Demo for switching between layouts

--- examples/basicgridlayout/index.js
+++ examples/basicgridlayout/index.js
@@ -2,6 +2,7 @@ var _ = require("lodash");
 var Velocity = require('velocity-animate');
 var decks = require('../..');
 var BasicGridLayout = decks.layouts.BasicGridLayout;
+var BasicStackLayout = decks.layouts.BasicStackLayout;
 
 var items = _.map(_.range(100), function(i) {
   return {
@@ -10,10 +11,23 @@ var items = _.map(_.range(100), function(i) {
   };
 });
 
-var layout = new BasicGridLayout();
+var layouts = {
+  grid: new BasicGridLayout({
+    itemWidth: 100,
+    itemHeight: 80,
+    itemPadding: 10,
+    itemsPerRow: 10
+  }),
+  stack: new BasicStackLayout({
+    itemWidth: 100,
+    itemHeight: 80,
+    itemPadding: 40,
+    itemsPerRow: 10
+  })
+};
 
 var loadRender = function (options) {
-  var imgUrl = options.item.get('imgUrl') + "/" + options.layout.itemWidth + "/" + options.layout.itemHeight + "/";
+  var imgUrl = options.item.get('imgUrl') + "/" + options.layout.itemWidth + "/" + options.layout.itemHeight + "/"; //"/?" + Math.random();
   options.newRender.element.innerHTML = "<div><img src='" + imgUrl + "' /></div>";
   options.item.isLoaded = true;
 };
@@ -23,17 +37,34 @@ var unloadRender = function (options) {
   options.item.isLoaded = false;
 };
 
-var myDeck = new decks.Deck({
-  viewport: {
-    animate: Velocity,
-    frame: document.body
-  },
-  layout: layout,
-  itemRenderer: {
-    loadRender: loadRender,
-    unloadRender: unloadRender
-  }
+
+$(function() {
+  var $layoutSelect = $(".select-layout");
+
+  var layout = layouts[$layoutSelect.val()];
+
+  var myDeck = new decks.Deck({
+    viewport: {
+      animate: Velocity,
+      frame: document.body
+    },
+    layout: layout,
+    itemRenderer: {
+      loadRender: loadRender,
+      unloadRender: unloadRender
+    }
+  });
+
+  myDeck.addItems(items, {silent: true});
+
+  myDeck.layout.draw();
+
+  $layoutSelect.on("change", function(e) {
+    var val = $layoutSelect.val();
+    console.log(val);
+
+    var layout = layouts[val];
+    myDeck.setLayout(layout);
+  });
 });
 
-myDeck.addItems(items, {silent: true});
-myDeck.layout.draw();
--- examples/stacked/index.js
+++ examples/stacked/index.js
@@ -39,15 +39,21 @@ var getRenders = function (options) {
 };
 
 var loadRender = function (options) {
+  if (options.newRender.isLoaded) {
+    return;
+  }
   var imgUrl = options.item.get('imgUrl');
   options.newRender.element.style.zIndex = options.index;
   options.newRender.element.innerHTML = "<div><img src='" + imgUrl + "200/140/' style=\"padding:8px 8px 24px 8px;background-color:#fff;box-shadow:4px 4px 2px rgba(0,0,0,0.5),0px 0px 20px rgba(0,0,0,0.5)\"/></div>";
-  options.item.isLoaded = true;
+  options.newRender.isLoaded = true;
 };
 
 var unloadRender = function (options) {
+  if (!options.newRender.isLoaded) {
+    return;
+  }
   options.newRender.element.innerHTML = "";
-  options.item.isLoaded = false;
+  options.newRender.isLoaded = false;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
