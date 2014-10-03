01ae4ef91b702dee467f656697de797e06a369fb
Andy White <andy.white@pellucid.com>
2014-10-03 16:41:24 -0600

    Work on multi example

--- examples/multi/index.js
+++ examples/multi/index.js
@@ -6,42 +6,52 @@ var Deck = decks.Deck;
 var BasicGridLayout = decks.layouts.BasicGridLayout;
 var BasicStackLayout = decks.layouts.BasicStackLayout;
 
-var items = _.map(_.range(20), function(i) {
+var i = -1;
+
+function createItem() {
+  i++;
   return {
-    index: i,
+    width: 300,
+    height: 200,
     imgUrl: "http://lorempixel.com",
     label: "Image " + i
   };
-});
+}
+
+var items = _.map(_.range(20), createItem);
 
 var loadRender = function (options) {
   var item = options.item;
   var render = options.render;
 
-  if (render.isLoading || render.isLoaded) {
+  var shouldLoad = !render.isLoading &&
+    (render.transform.width != render.lastWidth) &&
+    (render.transform.height != render.lastHeight);
+
+  if (!shouldLoad) {
     return;
   }
 
   render.isLoading = true;
-  render.isLoaded = false;
 
-  var imgUrl = item.get('imgUrl') + "/" + this.itemWidth + "/" + this.itemHeight + "/"; //"/?" + Math.random();
+  var imgUrl = item.get('imgUrl') + "/" + item.get("width") + "/" + item.get("height") + "/"; //"/?" + Math.random();
 
   var img = new Image(this.itemWidth, this.itemHeight);
+
   img.onload = _.bind(function() {
-    console.log("render loaded");
-
+    //console.log("render loaded");
     render.isLoading = false;
-    render.isLoaded = true;
+    render.lastWidth = render.transform.width;
+    render.lastHeight = render.transform.height;
+    render.element.innerHTML = "";
+    render.element.appendChild(img);
   }, this);
-  img.src = imgUrl;
 
-  render.element.innerHTML = "";
-  render.element.appendChild(img);
+  img.src = imgUrl;
 };
 
 var unloadRender = function (options) {
-  console.log("unloadRender");
+  //console.log("unloadRender");
   options.newRender.element.innerHTML = "";
   options.item.isLoaded = false;
 };
@@ -53,7 +63,12 @@ var layouts = {
     itemPadding: 10,
     itemsPerRow: 9,
     loadRender: loadRender,
-    unloadRender: unloadRender
+    unloadRender: unloadRender,
+    transform: {
+      rotateZ: 1080
+    },
+    animateOptions: {
+    }
   }),
   grid2: new BasicGridLayout({
     itemWidth: 150,
@@ -61,13 +76,18 @@ var layouts = {
     itemPadding: 15,
     itemsPerRow: 6,
     loadRender: loadRender,
-    unloadRender: unloadRender
+    unloadRender: unloadRender,
+    transform: {
+      rotateZ: 360
+    },
+    animateOptions: {
+    }
   }),
   stack: new BasicStackLayout({
-    itemWidth: 100,
-    itemHeight: 80,
+    itemWidth: 200,
+    itemHeight: 160,
     itemPadding: 40,
-    itemsPerRow: 9,
+    itemsPerRow: 4,
     loadRender: loadRender,
     unloadRender: unloadRender
   })
@@ -88,6 +108,33 @@ $(function() {
     var layout = layouts[val];
     deck.setLayout(layout);
   });
+
+  var $addItemButton = $(".add-item-button")
+    .on("click", function(e) {
+      console.log("add item");
+      deck.addItem(createItem(items.length));
+    });
+
+  var $removeItemButton = $(".remove-item-button")
+    .on("click", function(e) {
+      console.log("remove item");
+      var items = deck.itemCollection.getItems();
+      var index = Math.floor(Math.random() * (items.length - 1));
+      var item = items[index];
+      deck.removeItem(item);
+    });
+
+  var $clearItemsButton = $(".clear-items-button")
+    .on("click", function(e) {
+      console.log("clear");
+      deck.clear();
+    });
+
+  var $addItemsButton = $(".add-items-button")
+    .on("click", function(e) {
+      console.log("add items");
+      deck.addItems(items);
+    });
 
   var layout = layouts[$layoutSelect.val()];
 
