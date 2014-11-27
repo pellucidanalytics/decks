b496d42cc24d2b33d8329b687bc2de883f7824e6
Andy White <greatwhitehope@gmail.com>
2014-11-26 17:04:37 -0700

    Debounce window resize/scroll events, and do not queue movement animations in GestureHandler

--- examples/multi/index.js
+++ examples/multi/index.js
@@ -236,7 +236,7 @@ $(function() {
     config: {
       debugEvents: false,
       debugDrawing: false,
-      debugGestures: true
+      debugGestures: false
     },
     animator: {
       animate: Velocity
